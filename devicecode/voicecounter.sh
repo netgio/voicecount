4da22e519a95cb32d606d0937c54bc6e0bac02ad
netgio <gary@netgio.com>
2016-07-13 18:33:43 +0100

    added supervisord notes/config

--- devicecode/voicecounter.sh
+++ devicecode/voicecounter.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 cd /home/pi/voicecount
-python voicecounter.py &> output.log &
+
+export GST_PLUGIN_PATH=/usr/local/lib/gstreamer-1.0
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+## reset the USB microphone by disabling it and reenabling it.
+sudo sh -c "echo 0 > /sys/bus/usb/devices/1-1.5/authorized"
+sudo sh -c "echo 1 > /sys/bus/usb/devices/1-1.5/authorized"
+
+## run the voice counter and pipe output to a logfile.
+##python voicecounter.py &> output.log &
+
+## run the voice counter.
+python voicecounter.py
+
 exit 0
